d56ba117519b7b705fbe16334384180f6db354eb
chaitanyak <navchaitanya@gmail.com>
2025-04-28 09:16:39 +0530

    Updated IAC code

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
 output "vm_id" {
   value = azurerm_virtual_machine.this.id
 }
-
-output "vm_private_ip" {
-  value = azurerm_network_interface.this.private_ip_address
-}
-
-output "vm_nic_id" {
-  value = azurerm_network_interface.this.id
-}
